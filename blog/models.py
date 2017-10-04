0bfd2bd8b260253c0ac41768b17e8f367f30a1c8
Taimuraz <parabotic@gmail.com>
2017-10-04 17:31:15 +0300

    added form to new page creation

--- blog/models.py
+++ blog/models.py
@@ -8,6 +8,7 @@ class Post(models.Model):
     text = models.TextField()
     created_date = models.DateTimeField(default=timezone.now)
     published_date = models.DateTimeField(blank=True, null=True)
+    preview = models.TextField(default="No preview.")
 
     def publish(self):
         self.published_date = timezone.now()
